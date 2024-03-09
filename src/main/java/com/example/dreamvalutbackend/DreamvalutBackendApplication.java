3e0d2fc756a6309264185de8b9904af0662184b0
kwongwangjae <kgj9906@naver.com>
2024-03-10 04:37:13 +0900

    feat: 기본 스프링부트 인텔라이져 생성

---
+++ src/main/java/com/example/dreamvalutbackend/DreamvalutBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.dreamvalutbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamvalutBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DreamvalutBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dreamvalutbackend/DreamvalutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dreamvalutbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamvalutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
