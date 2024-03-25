68d0cb7a19ea489d5fde08d5b9eb9c3fe1783f82
gerry-mandering <kimmen1119@gmail.com>
2024-03-25 17:02:22 +0900

    Feat: `Genre` 엔티티 설계
    - `id` : `genre_id`로 Column명 설정
    - `name` : 장르의 명은 최대 50자로 설정

---
+++ src/main/java/com/example/dreamvalutbackend/domain/genre/domain/Genre.java
@@ -0,0 +1,23 @@
+package com.example.dreamvalutbackend.domain.genre.domain;
+
+import com.example.dreamvalutbackend.domain.common.BaseTimeEntity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "genres")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Genre extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "genre_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}
