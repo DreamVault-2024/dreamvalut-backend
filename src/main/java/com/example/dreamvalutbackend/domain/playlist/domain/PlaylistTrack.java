929625b793d9d016af3e84d7766b6f3d29c98889
gerry-mandering <kimmen1119@gmail.com>
2024-03-27 10:35:49 +0900

    Feat: `Playlist`, `PlaylistTrack`, MyPlaylist` 엔티티 설계

---
+++ src/main/java/com/example/dreamvalutbackend/domain/playlist/domain/PlaylistTrack.java
@@ -0,0 +1,29 @@
+package com.example.dreamvalutbackend.domain.playlist.domain;
+
+import com.example.dreamvalutbackend.domain.track.domain.Track;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "playlist_tracks")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlaylistTrack {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "playlist_track_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "playlist_id")
+    private Playlist playlist;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "track_id")
+    private Track track;
+}
