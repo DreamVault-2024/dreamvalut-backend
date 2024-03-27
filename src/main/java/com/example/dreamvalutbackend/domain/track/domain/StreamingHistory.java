1771544188de911be1f3aa15806d0b119e85797f
gerry-mandering <kimmen1119@gmail.com>
2024-03-27 10:37:23 +0900

    Remove: `StreamingHistory` 엔티티의 `streamingDuration` 필드 제거

--- src/main/java/com/example/dreamvalutbackend/domain/track/domain/StreamingHistory.java
+++ src/main/java/com/example/dreamvalutbackend/domain/track/domain/StreamingHistory.java
@@ -19,10 +19,6 @@ public class StreamingHistory extends BaseTimeEntity {
     @Column(name = "streaming_history_id")
     private Long id;
 
-    // 스트리밍 시간(초) - 측정 어려울 시, 해당 필드 사용 안 할 수 있음
-    @Column(nullable = false)
-    private Integer streamingDuration;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
