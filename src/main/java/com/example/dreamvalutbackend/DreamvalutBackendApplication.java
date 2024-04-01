bb620154c6a4a9a551504b397b6f826a74829dae
gerry-mandering <kimmen1119@gmail.com>
2024-04-01 21:13:47 +0900

    Feat: `S3ClientProperties` 프로퍼티 객체 구현 및 Application에서 프로퍼티 스캔 활성화

--- src/main/java/com/example/dreamvalutbackend/DreamvalutBackendApplication.java
+++ src/main/java/com/example/dreamvalutbackend/DreamvalutBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.dreamvalutbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("com.example.dreamvalutbackend.config.aws")
 public class DreamvalutBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DreamvalutBackendApplication.class, args);
