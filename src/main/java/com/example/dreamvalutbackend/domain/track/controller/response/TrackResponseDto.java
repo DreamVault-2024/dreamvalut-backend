17380fc4b130818f59f0d5d580b3943f0385b05b
gerry-mandering <kimmen1119@gmail.com>
2024-04-03 17:28:47 +0900

    Feat: `TrackResponseDto` 구현

---
+++ src/main/java/com/example/dreamvalutbackend/domain/track/controller/response/TrackResponseDto.java
@@ -0,0 +1,46 @@
+package com.example.dreamvalutbackend.domain.track.controller.response;
+
+import com.example.dreamvalutbackend.domain.track.domain.Track;
+import com.example.dreamvalutbackend.domain.track.domain.TrackDetail;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class TrackResponseDto {
+
+    private Long trackId;
+    private String title;
+    private String uploaderName;
+    private Boolean hasLyrics;
+    private String trackUrl;
+    private String trackImage;
+    private String thumbnailImage;
+    private String prompt;
+
+    @Builder
+    public TrackResponseDto(Long trackId, String title, String uploaderName, Boolean hasLyrics, String trackUrl,
+            String trackImage, String thumbnailImage, String prompt) {
+        this.trackId = trackId;
+        this.title = title;
+        this.uploaderName = uploaderName;
+        this.hasLyrics = hasLyrics;
+        this.trackUrl = trackUrl;
+        this.trackImage = trackImage;
+        this.thumbnailImage = thumbnailImage;
+        this.prompt = prompt;
+    }
+
+    public static TrackResponseDto toDto(Track track, TrackDetail trackDetail) {
+        return TrackResponseDto.builder()
+                .trackId(track.getId())
+                .title(track.getTitle())
+                .uploaderName(track.getUser().getDisplayName())
+                .hasLyrics(track.getHasLyrics())
+                .trackUrl(track.getTrackUrl())
+                .trackImage(track.getTrackImage())
+                .thumbnailImage(track.getThumbnailImage())
+                .prompt(trackDetail.getPrompt())
+                .build();
+    }
+}
