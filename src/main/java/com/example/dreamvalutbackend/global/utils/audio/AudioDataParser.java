b5c35a0edaa6c4c41954993d8ed9251ff846b85b
gerry-mandering <kimmen1119@gmail.com>
2024-04-04 15:12:40 +0900

    Comment: `AudioDataParser` 주석 추가

--- src/main/java/com/example/dreamvalutbackend/global/utils/audio/AudioDataParser.java
+++ src/main/java/com/example/dreamvalutbackend/global/utils/audio/AudioDataParser.java
@@ -17,15 +17,18 @@ public class AudioDataParser {
     public static int extractDurationInSeconds(MultipartFile audioFile) throws IOException {
         Path tempFile = null;
         try {
+            // 임시 파일 생성
             tempFile = Files.createTempFile("temp", audioFile.getOriginalFilename());
             Files.copy(audioFile.getInputStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
 
+            // 임시 파일로부터 음악 길이 추출
             AudioFile f = AudioFileIO.read(tempFile.toFile());
             return f.getAudioHeader().getTrackLength();
         } catch (Exception e) {
             log.error("Failed to extract audio duration", e);
             return 0;
         } finally {
+            // 임시 파일 삭제
             if (tempFile != null && Files.exists(tempFile)) {
                 Files.delete(tempFile);
             }
