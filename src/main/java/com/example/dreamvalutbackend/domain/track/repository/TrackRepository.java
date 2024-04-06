efa9fd9f72f7d8009eda093cef59f119e559cd3d
gerry-mandering <kimmen1119@gmail.com>
2024-04-06 19:17:41 +0900

    Feat: `TrackRepository` Pagination을 적용한 findAllByGenreId() 메소드 작성

--- src/main/java/com/example/dreamvalutbackend/domain/track/repository/TrackRepository.java
+++ src/main/java/com/example/dreamvalutbackend/domain/track/repository/TrackRepository.java
@@ -1,5 +1,7 @@
 package com.example.dreamvalutbackend.domain.track.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.example.dreamvalutbackend.domain.track.domain.Track;
 @Repository
 public interface TrackRepository extends JpaRepository<Track, Long> {
 
+    Page<Track> findAllByGenreId(Long genreId, Pageable pageable);
 }
