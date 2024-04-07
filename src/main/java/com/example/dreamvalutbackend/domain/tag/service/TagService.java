1cda9e0e73e1ed92a4b35cb0f73cfb797f554cb5
gerry-mandering <kimmen1119@gmail.com>
2024-04-07 17:02:20 +0900

    Feat: `모든 태그 리스트 가져오기` API 구현

--- src/main/java/com/example/dreamvalutbackend/domain/tag/service/TagService.java
+++ src/main/java/com/example/dreamvalutbackend/domain/tag/service/TagService.java
@@ -1,9 +1,13 @@
 package com.example.dreamvalutbackend.domain.tag.service;
 
 import java.util.Arrays;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.dreamvalutbackend.domain.tag.controller.response.TagResponseDto;
 import com.example.dreamvalutbackend.domain.tag.domain.Tag;
 import com.example.dreamvalutbackend.domain.tag.domain.TrackTag;
 import com.example.dreamvalutbackend.domain.tag.repository.TagRepository;
@@ -19,11 +23,16 @@ public class TagService {
     private final TagRepository tagRepository;
     private final TrackTagRepository trackTagRepository;
 
+    public Page<TagResponseDto> listAllTags(Pageable pageable) {
+        return tagRepository.findAll(pageable).map(TagResponseDto::toDto);
+    }
+
     @Transactional
     public void associateTrackWithTags(Track track, String[] tagNames) {
         // 태그들을 순회하며 태그를 찾아서 없으면 생성하고, TrackTag를 생성하여 저장
         Arrays.stream(tagNames).forEach(tagName -> {
-            Tag tag = tagRepository.findByTagName(tagName).orElseGet(() -> tagRepository.save(new Tag(tagName)));
+            Tag tag = tagRepository.findByTagName(tagName)
+                    .orElseGet(() -> tagRepository.save(new Tag(tagName, "default tag image")));
             trackTagRepository.save(new TrackTag(track, tag));
         });
     }
