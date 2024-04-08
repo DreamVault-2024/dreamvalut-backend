76ba037a2805e27a86ed1aea06921042efef78ad
gerry-mandering <kimmen1119@gmail.com>
2024-04-08 23:17:56 +0900

    Feat: `PlaylistTrackRepository` - Pagination 적용한 `findAllByPlaylistId() 작성

--- src/main/java/com/example/dreamvalutbackend/domain/playlist/repository/PlaylistTrackRepository.java
+++ src/main/java/com/example/dreamvalutbackend/domain/playlist/repository/PlaylistTrackRepository.java
@@ -1,9 +1,15 @@
 package com.example.dreamvalutbackend.domain.playlist.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.dreamvalutbackend.domain.playlist.domain.PlaylistTrack;
 
 public interface PlaylistTrackRepository extends JpaRepository<PlaylistTrack, Long> {
 
+    @Query("SELECT pt FROM PlaylistTrack pt WHERE pt.playlist.id = :playlistId ORDER BY pt.createdAt DESC")
+    Page<PlaylistTrack> findAllByPlaylistId(@Param("playlistId") Long playlistId, Pageable pageable);
 }
