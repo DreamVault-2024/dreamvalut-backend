e319e7b419f6156ca22d177162979e4dbac95890
gerry-mandering <kimmen1119@gmail.com>
2024-04-09 15:15:13 +0900

    Remove: 중복 import 삭제

--- src/test/java/com/example/dreamvalutbackend/domain/playlist/service/PlaylistServiceTest.java
+++ src/test/java/com/example/dreamvalutbackend/domain/playlist/service/PlaylistServiceTest.java
@@ -3,12 +3,10 @@ package com.example.dreamvalutbackend.domain.playlist.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,12 +20,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
-import com.example.dreamvalutbackend.domain.genre.domain.Genre;
 import com.example.dreamvalutbackend.domain.genre.domain.Genre;
 import com.example.dreamvalutbackend.domain.playlist.controller.request.CreatePlaylistRequestDto;
 import com.example.dreamvalutbackend.domain.playlist.controller.request.UpdatePlaylistNameRequestDto;
@@ -36,13 +29,8 @@ import com.example.dreamvalutbackend.domain.playlist.controller.response.Playlis
 import com.example.dreamvalutbackend.domain.playlist.domain.MyPlaylist;
 import com.example.dreamvalutbackend.domain.playlist.domain.Playlist;
 import com.example.dreamvalutbackend.domain.playlist.domain.PlaylistTrack;
-import com.example.dreamvalutbackend.domain.playlist.domain.PlaylistTrack;
 import com.example.dreamvalutbackend.domain.playlist.repository.MyPlaylistRepository;
 import com.example.dreamvalutbackend.domain.playlist.repository.PlaylistRepository;
-import com.example.dreamvalutbackend.domain.playlist.repository.PlaylistTrackRepository;
-import com.example.dreamvalutbackend.domain.track.domain.Track;
-import com.example.dreamvalutbackend.domain.track.domain.TrackDetail;
-import com.example.dreamvalutbackend.domain.track.repository.TrackDetailRepository;
 import com.example.dreamvalutbackend.domain.playlist.repository.PlaylistTrackRepository;
 import com.example.dreamvalutbackend.domain.track.domain.Track;
 import com.example.dreamvalutbackend.domain.track.domain.TrackDetail;
