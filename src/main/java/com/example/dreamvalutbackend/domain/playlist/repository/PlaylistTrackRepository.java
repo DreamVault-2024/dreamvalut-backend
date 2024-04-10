ebe85f54beea78741e45ba9a058a4eb41f287fd1
gerry-mandering <kimmen1119@gmail.com>
2024-04-10 13:27:53 +0900

    Feat: `MyPlaylistRepository`, `PlaylistTrackRepository` 에서 플레이리스트 삭제시 호출되는 `deleteByPlaylist()` 메소드 추가

--- src/main/java/com/example/dreamvalutbackend/domain/playlist/repository/PlaylistTrackRepository.java
+++ src/main/java/com/example/dreamvalutbackend/domain/playlist/repository/PlaylistTrackRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.example.dreamvalutbackend.domain.playlist.domain.Playlist;
 import com.example.dreamvalutbackend.domain.playlist.domain.PlaylistTrack;
 
 public interface PlaylistTrackRepository extends JpaRepository<PlaylistTrack, Long> {
 
     @Query("SELECT pt FROM PlaylistTrack pt WHERE pt.playlist.id = :playlistId ORDER BY pt.createdAt DESC")
     Page<PlaylistTrack> findAllByPlaylistId(@Param("playlistId") Long playlistId, Pageable pageable);
+
+    void deleteByPlaylist(Playlist playlist);
 }
