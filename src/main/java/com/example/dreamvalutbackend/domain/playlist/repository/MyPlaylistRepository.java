34115b15241458e1d5a2fc995ec88efe91c368b3
gerry-mandering <kimmen1119@gmail.com>
2024-04-11 13:14:20 +0900

    Feat: `MyPlaylistRepository` - `existsByUserAndPlaylist()`, `findByUserAndPlaylist()` 추가

--- src/main/java/com/example/dreamvalutbackend/domain/playlist/repository/MyPlaylistRepository.java
+++ src/main/java/com/example/dreamvalutbackend/domain/playlist/repository/MyPlaylistRepository.java
@@ -1,11 +1,18 @@
 package com.example.dreamvalutbackend.domain.playlist.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.dreamvalutbackend.domain.playlist.domain.MyPlaylist;
 import com.example.dreamvalutbackend.domain.playlist.domain.Playlist;
+import com.example.dreamvalutbackend.domain.user.domain.User;
 
 public interface MyPlaylistRepository extends JpaRepository<MyPlaylist, Long> {
 
     void deleteByPlaylist(Playlist playlist);
+
+    boolean existsByUserAndPlaylist(User user, Playlist playlist);
+
+    Optional<MyPlaylist> findByUserAndPlaylist(User user, Playlist playlist);
 }
